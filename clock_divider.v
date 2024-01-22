73a1d64cb974d385bdc435c6551f54b7dc477b68
gitmonkeys <127384845+gitmonkeys@users.noreply.github.com>
2024-01-22 12:39:59 -0700

    clock_divider.v

--- clock_divider.v
+++ clock_divider.v
@@ -1,4 +1,23 @@
 module clock_divider { input clk, reset,
                        output reg sclk
                      };
-  reg [31:0] count
+  reg [31:0] count;
+
+  always@(posedge clk or posedge reset)
+    begin
+      if (reset == 1'b1) begin
+        count <= 32'd0;
+        sclk <= 1'b0;
+      end else 
+        begin
+          if (count == 32'd100000000) begin // 2 sec
+            count <= 32'd0;
+            sclk <= ~sclk;
+          end else 
+            begin
+              count <= count + 1;
+            end
+        end
+    end
+endmodule
+            
