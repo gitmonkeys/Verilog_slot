cc6a473f351bdd30eb65444ac115ad289f490a18
gitmonkeys <127384845+gitmonkeys@users.noreply.github.com>
2024-01-22 10:58:29 -0700

    finaldesign.v

--- finaldesign.v
+++ finaldesign.v
@@ -1,3 +1,34 @@
 module finaldesign { input reset, clk, lever,
                      output [7:0] Anode_Activate,
-                     output [
+                     output [6:0] LED_out
+                   };
+  wire sclk;
+  wire [3:0] resultMSB, result2, resultLSB;
+  wire [3:0] score;
+
+  clock_divider CLKDIV (.clk(clk),
+                        .reset(reset),
+                        .sclk(sclk)
+                       );
+  
+  slotstate ST (.clk(sclk),
+                .reset(reset),
+                .resultMSB(resultMSB),
+                .result2(result2),
+                .resultLSB(resultLSB),
+                .score(score),
+                .lever(lever)
+               );
+
+  sevensegmentLED SLED (.clk(clk),
+                        .reset(reset),
+                        .resultMSB(resultMSB),
+                        .result2(result2),
+                        .resultLSB(resultLSB),
+                        .score(score),
+                        .Anode_Activate(Anode_Activatee),
+                        .LED_out(LED_out)
+                       );
+  
+                        
+                
